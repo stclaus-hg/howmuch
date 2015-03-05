9000d8747838420d034f8b35bdbb07b2b33766d0
Alexander Voitenko <voitenko@gmail.com>
2015-03-05 17:48:26 +0200

    Added cvs file with inflation data

--- app/views.py
+++ app/views.py
@@ -1,33 +1,28 @@
+import sys
 from app import app
 from app.forms import CalculatePriceForm
 from flask import request, render_template
 from decimal import Decimal, getcontext
+import csv
+import os
 
 getcontext().prec = 2
 
-inflation = {
-    2000: Decimal(1.258),
-    2001: Decimal(1.061),
-    2002: Decimal(0.994),
-    2003: Decimal(1.082),
-    2004: Decimal(1.123),
-    2005: Decimal(1.103),
-    2006: Decimal(1.116),
-    2007: Decimal(1.166),
-    2008: Decimal(1.223),
-    2009: Decimal(1.123),
-    2010: Decimal(1.091),
-    2011: Decimal(1.046),
-    2012: Decimal(0.998),
-    2013: Decimal(1.005),
-    2014: Decimal(1.249),
-}
+def load_inflation():
+    path =  os.path.dirname(os.path.realpath(__file__))
+    csv_path = os.path.join(path, '../inflation.csv')
+    with open(csv_path) as inflation:
+        reader = csv.reader(inflation, delimiter=',')
+        return {int(row[0]):row[1] for row in reader}
+
+inflation = load_inflation()
+print inflation
 
 def calculate_price(source_year, source_price, result_year):
     res = source_price
     if source_year < result_year:
         for year in range(source_year, result_year):
-            res *= inflation[year]
+            res *= Decimal(inflation[year])
             print Decimal(res)
     else:
         for year in range(result_year, source_year):
