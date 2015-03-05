1ff39d753da4f52d435a80f09eddece6e7f44219
Alexander Voitenko <voitenko@gmail.com>
2015-03-05 17:25:28 +0200

    Fixed run script

--- runp-heroku.py
+++ runp-heroku.py
@@ -3,5 +3,7 @@
 from app import app
 import os
 
-port = int(os.environ.get('PORT', 33507))
-app.run(debug=False, host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=False, host='0.0.0.0', port=port)
