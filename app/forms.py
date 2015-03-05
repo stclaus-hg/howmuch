f4e25d42ebe349a7e6386ed5f56c72b8c98d3429
Alexander Voitenko <voitenko@gmail.com>
2015-03-05 13:30:50 +0200

    Added form fields description

--- app/forms.py
+++ app/forms.py
@@ -5,13 +5,13 @@ from wtforms.validators import DataRequired, Length, NumberRange
 from flask_wtf.html5 import IntegerField
 
 class CalculatePriceForm(Form):
-    source_year = IntegerField('In year',
+    source_year = IntegerField('In year', description='Between 2000 and 2015 years',
                                validators=[DataRequired(), NumberRange(min=2000,
                                                                        max=2015,
                                                                        message='Year must be between 2000 and 2015.')])
     source_price = DecimalField('it was cost', validators=[DataRequired()])
 
-    result_year = IntegerField('In Year',
+    result_year = IntegerField('In Year', description='Between 2000 and 2015 years',
                                validators=[DataRequired(), NumberRange(min=2000,
                                                                        max=2015,
                                                                        message='Year must be between 2000 and 2015.')])
